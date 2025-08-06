8d9080b2ba6d288fbeb030bfb9b4eebdf93419e3
DenisGodov1993 <denisgodovanyuk@gmail.com>
2025-08-06 11:18:50 +0300

    wrote code for 2 homework assignments

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,65 @@
+// import Column from "../Column/Column";
+
+// const Main = ({ loading }) => {
+//   const statuses = [
+//     "Без статуса",
+//     "Нужно сделать",
+//     "В работе",
+//     "Тестирование",
+//     "Готово",
+//   ];
+
+//   return (
+//     <main className="main">
+//       <div className="container">
+//         <div className="main__block">
+//           <div className="main__content">
+//             {statuses.map((title) => (
+//               <Column
+//                 key={title}
+//                 loading={loading}
+//                 title={title}
+//               />
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// };
+
+// export default Main;
+
+
+
+
 import Column from "../Column/Column";
 
-const Main = () => {
-  return (
+const Main = ({ loading }) => {
+  const statuses = [
+    "Без статуса",
+    "Нужно сделать",
+    "В работе",
+    "Тестирование",
+    "Готово",
+  ];
+
+  return loading ? (
+    <div className="loading">
+      <p className="loading-text">Данные загружаются . . . </p>
+    </div>
+  ) : (
     <main className="main">
       <div className="container">
         <div className="main__block">
           <div className="main__content">
-            <Column />
+            {statuses.map((title) => (
+              <Column
+                key={title}
+                loading={loading}
+                title={title}
+              />
+            ))}
           </div>
         </div>
       </div>
@@ -15,3 +68,22 @@ const Main = () => {
 };
 
 export default Main;
+
+
+// import Column from "../Column/Column";
+
+// const Main = () => {
+//   return (
+//     <main className="main">
+//       <div className="container">
+//         <div className="main__block">
+//           <div className="main__content">
+//             <Column />
+//           </div>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// };
+
+// export default Main;
