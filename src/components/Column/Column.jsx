66f2c44b54d8025d6b240c3434cb644785567845
DenisGodov1993 <denisgodovanyuk@gmail.com>
2025-08-13 13:01:20 +0300

    Styling the Column  with Styled Components

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,21 +1,21 @@
 import Card from "../Card/Card";
 import { cardList } from "../../data.js";
-// import { SHeader, Container } from "./Column.styled";
+import { SMainColumn, SColumnTitle, SCards } from "./Column.styled";
 
 const Column = ({ title }) => {
   return (
-    <div className="main__column">
-      <div className="column__title">
+    <SMainColumn>
+      <SColumnTitle>
         <p>{title}</p>
-      </div>
-      <div className="cards">
+      </SColumnTitle>
+      <SCards>
         {cardList
           .filter((card) => card.status === title)
           .map((card) => (
             <Card key={card.id} data={card} />
           ))}
-      </div>
-    </div>
+      </SCards>
+    </SMainColumn>
   );
 };
 
--- src/components/Column/Column.styled.js
+++ src/components/Column/Column.styled.js
@@ -1,14 +1,26 @@
 import styled from "styled-components";
 
-export const SHeader = styled.header`
-  width: 100%;
+export const SMainColumn = styled.div`
+  width: 20%;
   margin: 0 auto;
-  background-color: #ffffff;
+  display: block;
 `;
 
-export const Container = styled.div`
-  max-width: 1260px;
+export const SColumnTitle = styled.div`
+  padding: 0 10px;
+  margin: 15px 0;
+
+  & p {
+    color: #94a6be;
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 1;
+    text-transform: uppercase;
+  }
+`;
+
+export const SCards = styled.div`
   width: 100%;
-  margin: 0 auto;
-  padding: 0 30px;
+  display: block;
+  position: relative;
 `;
